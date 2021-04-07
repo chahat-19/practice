173b288156ccd8197fe4c67c310ba9279ca2e382
Chahat <chahat@nagarro.com>
2021-04-07 11:53:35 +0530

    first commit

---
+++ git.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is me practicing git");
+
+	}
+
+}
---
+++ git2.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class git2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just one more file");
+
+	}
+
+}
